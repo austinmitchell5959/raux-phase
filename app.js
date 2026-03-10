f9a5a90c4dfdbfb122052dc94f75968597a01e66
Austin Mitchell <austinmitchell5959@gamil.com>
2026-03-10 14:44:13 -0400

    Make terminal app ask three questions

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ const appName = "Raux Phase 2";
 
 console.log(`${appName} is running`);
 
-rl.question("What do you want to call this session? ", function(answer) {
-  console.log(`Session started: ${answer}`);
-  rl.close();
+rl.question("What do you want to call this session? ", function(sessionName) {
+  rl.question("What mode are we in? ", function(mode) {
+    rl.question("What is the mood? ", function(mood) {
+      console.log("\n--- Session Summary ---");
+      console.log(`App: ${appName}`);
+      console.log(`Session: ${sessionName}`);
+      console.log(`Mode: ${mode}`);
+      console.log(`Mood: ${mood}`);
+      rl.close();
+    });
+  });
 });
